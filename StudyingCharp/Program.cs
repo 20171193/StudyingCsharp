c8d4dbb987e69c69e9f363c2a11439d61f571ab7
Shim Jae Chun <shimnaldo98@gmail.com>
2024-01-27 16:21:09 +0900

    1388번 BFS 풀이 추가

--- StudyingCharp/Program.cs
+++ StudyingCharp/Program.cs
@@ -1,53 +1,121 @@
-﻿namespace StudyingCharp
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+// 백준 1388번 바닥 장식 C# 풀이 
+namespace BOJ_1388
 {
-    internal class Program
+    class BOJ1388
     {
-        public static int[] solution(int[] array, int[,] commands)
+        // 같은 로직 C# 풀이
+        // ______________________________
+        // |        |   C#    |    C++  |
+        // | 메모리 | 5712KB  |  2028KB |   
+        // |   시간 |  64ms   |    0ms  |
+        // ------------------------------
+
+        static int n = 0, m = 0, ret = 0;
+        static string[] arr;
+        static bool[,] visited;
+
+        public struct Position
         {
-            int[] answer = new int[commands.GetLength(0)];
-
-            for (int i = 0; i < commands.GetLength(0); i++)
+            public int x = 0;
+            public int y = 0;
+            public Position(int y, int x)
             {
-                int[] temp = new int[commands[i, 1] - commands[i, 0] + 1];
-
-                int index = 0;
-                for (int j = commands[i, 0] - 1; j < commands[i, 1]; j++, index++)
-                {
-                    temp[index] = array[j];
-                }
-
-                foreach (int j in temp)
-                {
-                    Console.Write(j);
-                }
-                Console.WriteLine();
-
-                Array.Sort(temp);
-
-                foreach (int j in temp)
-                {
-                    Console.Write(j);
-                }
-                Console.WriteLine();
-
-                //answer[i] = temp[commands[i,2]];
+                this.x = x;
+                this.y = y;
             }
-            return answer;
+        }
+        // 깊이 우선
+        static void DFS(int y, int x, char c)
+        {
+            if (x >= m || y >= n) 
+                return;
+            if (visited[y, x]) 
+                return;
+            if (arr[y][x] != c) 
+                return;
+
+            visited[y, x] = true;
+
+            if (c == '-')
+                DFS(y, x + 1, c);
+            else // c == '|'
+                DFS(y + 1, x, c);
+        }
+        
+        // 너비 우선
+        static void BFS(int y, int x, char c)
+        {
+            Queue<Position> q = new Queue<Position>();
+            visited[y, x] = true;
+            q.Enqueue(new Position(y, x));
+
+            while(q.Count > 0)
+            {
+                Position curPos = q.Dequeue();
+                if(c == '-')    // 가로일 때
+                {
+                    if (curPos.x + 1 >= m) 
+                        continue;
+                    if (visited[curPos.y, curPos.x + 1] == true) 
+                        continue;
+                    if (arr[curPos.y][curPos.x+1] != c) 
+                        continue;
+
+                    q.Enqueue(new Position(curPos.y, curPos.x + 1));
+                    visited[curPos.y, curPos.x+1] = true;
+                }
+                else // 세로일 때
+                {
+                    if (curPos.y + 1 >= n) 
+                        continue;
+                    if (visited[curPos.y+1, curPos.x] == true) 
+                        continue;
+                    if (arr[curPos.y+1][curPos.x] != c) 
+                        continue;
+
+                    q.Enqueue(new Position(curPos.y+1, curPos.x));
+                    visited[curPos.y+1, curPos.x] = true;
+                }
+            }
         }
 
-
-        static void Main(string[] args)
+        static void Main(string[] argc)
         {
-            Console.WriteLine("Hello, World!");
+            string[] temp = Console.ReadLine().Split(' ');
+            n = int.Parse(temp[0]);
+            m = int.Parse(temp[1]);
 
-            int[] arr1 = new int[7] { 1, 5, 2, 6, 3, 7, 4 };
-            int[,] arr2 = new int[3, 3]
+            arr = new string[n];
+            visited = new bool[n, m];
+
+            for (int i = 0; i < n; i++)
             {
-                { 2,5,3},
-                { 4,4,1},
-                { 1,7,3}
-            };
-            solution(arr1, arr2);
+                arr[i] = Console.ReadLine();
+            }
+
+            int answer = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (!visited[i, j])
+                    {
+                        answer++;
+                        BFS(i, j, arr[i][j]);
+                    }
+                }
+            }
+            Console.WriteLine(answer);
         }
     }
 }
+
